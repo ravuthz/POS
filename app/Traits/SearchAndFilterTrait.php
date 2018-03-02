10cd501a209d4f806f43b3d36ee7678e1c81bd73
Sai Vichet <v.sai@gl-f.com>
2018-03-02 10:02:48 +0700

    front sidebar fix style

--- app/Traits/SearchAndFilterTrait.php
+++ app/Traits/SearchAndFilterTrait.php
@@ -2,15 +2,17 @@
 
 namespace App\Traits;
 
-trait SearchAndFilterTrait {
-    
-    public function scopeSearchName($query, $name = '') {
+trait SearchAndFilterTrait
+{
+    public function scopeSearchName($query, $name = '')
+    {
         $query->where('name', 'like', "%{$name}%");
         $query->orWhere('name_kh', 'like', "%{$name}%");
         return $query;
     }
-    
-    public function scopeFilterName($query, $request) {
+
+    public function scopeFilterName($query, $request)
+    {
         if ($request->has('name')) {
             $name = $request->get('name');
             $query->where('name', 'like', "%{$name}%");
@@ -18,28 +20,28 @@ trait SearchAndFilterTrait {
         }
         return $query;
     }
-    
-    public function scopeGetNamePageSort($query, $request) {
+
+    public function scopeGetNamePageSort($query, $request)
+    {
         $size = $request->get('size', '12');
         $sort = $request->get('sort', 'null');
         $desc = $request->get('desc', 'false');
         $filter = $request->get('filter', 'null');
-        
+
         if ($sort === null || $sort == 'null') {
             $sort = 'id';
         }
-        
+
         if ($desc == false || $desc == 'false') {
             $desc = 'asc';
         } else {
             $desc = 'desc';
         }
-        
+
         if ($filter == null || $filter == 'null') {
             $filter = '';
         }
-        
+
         return $query->searchName($filter)->orderBy($sort, $desc)->paginate($size);
     }
-    
 }
