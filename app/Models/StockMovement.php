bed2f45403f53dee9059d32a193a07e78abaf209
Sai Vichet <v.sai@gl-f.com>
2018-03-22 16:41:12 +0700

    fix badge

--- app/Models/StockMovement.php
+++ app/Models/StockMovement.php
@@ -7,14 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class StockMovement extends Model
 {
     protected $fillable = ['stock_id', 'product_id', 'price', 'quantity'];
-    
+
     public function stock()
     {
         return $this->belongsTo(Stock::class, 'stock_id');
     }
-    
+
     public function product()
     {
-        return $this->belongsTo(Product::class, 'product_id');    
+        return $this->belongsTo(Product::class, 'product_id');
     }
 }
