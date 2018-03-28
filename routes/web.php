947c81898a8c709cf5df065164f3eaa75f62cbf4
Ravuthz <ravuthz@gmail.com>
2018-03-28 14:21:43 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::get('/logout', function () {
 
 Route::group([
     'prefix'    => 'api',
-    'namespace' => 'Api'
-    // 'middleware' => 'auth'
+    'namespace' => 'Api',
+     'middleware' => 'auth'
 ], function () {
     Route::apiResource('products', 'ProductController');
     Route::get('sales-products', 'ProductController@sales');
