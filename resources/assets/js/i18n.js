5b831f5df984c3843b9d96690f8a8a81ca758614
Ravuthz <ravuthz@gmail.com>
2018-04-06 11:56:51 +0700

    Done Khmer fonts

--- resources/assets/js/i18n.js
+++ resources/assets/js/i18n.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 import en from './lang/en.json';
+import kh from './lang/kh.json';
 
 Vue.use(VueI18n);
-const locale = 'en';
+const locale = 'kh';
 
 const messages = {
-    en: en
+    en: en,
+    kh: kh
 };
 
 const i18n = new VueI18n({
